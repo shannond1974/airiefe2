033ce00f37cb12a770460f8a26072c595bfc48eb
shannond1 <daniel.shannon1@mbie.govt.nz>
2024-06-05 21:19:56 +1200

    next commmit here

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
